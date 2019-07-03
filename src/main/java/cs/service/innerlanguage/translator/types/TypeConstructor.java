0fbba5a44b325401037884e9c13aa46084421af7
Anisimov Alexey <anisimov_a_v@ntik.ru>
2019-07-03 11:58:51 +0300

    add BasicTypesProvider and base types for it

--- src/main/java/cs/service/innerlanguage/translator/types/TypeConstructor.java
+++ src/main/java/cs/service/innerlanguage/translator/types/TypeConstructor.java
@@ -5,8 +5,12 @@
  */
 package cs.service.innerlanguage.translator.types;
 
+import cs.service.innerlanguage.translator.types.basic.BaseTypeConstructorView;
 import cs.service.innerlanguage.utils.Pair;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  *
@@ -17,6 +21,19 @@ public class TypeConstructor {
 
 	public TypeConstructor(List<Pair<TypeWrapper, String>> parameters) {
 		this.parameters = parameters;
+	}
+
+	public TypeConstructor(BaseTypeConstructorView constructorView, Map<String, TypeWrapper> typesByClassName) {
+		this.parameters = (constructorView != null && constructorView.getParameters() != null)
+								? constructorView
+				  .getParameters()
+				  .entrySet()
+				  .stream()
+				  .map(entry -> {
+					  return new Pair<>(typesByClassName.get(entry.getValue()), entry.getKey());
+				  })
+				  .collect(Collectors.toList())
+								: new ArrayList();
 	}
 
 	public List<Pair<TypeWrapper, String>> getParameters() {
--- src/main/java/cs/service/innerlanguage/translator/types/basic/BasicProvider.java
+++ src/main/java/cs/service/innerlanguage/translator/types/basic/BasicProvider.java
@@ -6,13 +6,16 @@
 package cs.service.innerlanguage.translator.types.basic;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import cs.service.innerlanguage.translator.types.TypeWrapper;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -23,8 +26,12 @@ import java.util.logging.Logger;
 public final class BasicProvider {
 	private static final String TYPEPATH = "types";
 	private static BasicProvider instance;
+	private final Map<String, TypeWrapper> typesByName;
+	private final Map<String, TypeWrapper> typesByClassName;
 
 	private BasicProvider() {
+		this.typesByName = new HashMap();
+		this.typesByClassName = new HashMap();
 		reload();
 	}
 
@@ -39,15 +46,44 @@ public final class BasicProvider {
 		return instance;
 	}
 
+	public Map<String, TypeWrapper> getTypesByClassName() {
+		return typesByClassName;
+	}
+
+	public Map<String, TypeWrapper> getTypesByName() {
+		return typesByName;
+	}
+
 	public void reload() {
 		try {
 			for (String name : getResourceFiles(TYPEPATH)) {
 				ObjectMapper mapper = new ObjectMapper();
 				TypesTreeView view = mapper.readValue(getContextClassLoader().getResourceAsStream(TYPEPATH + File.separator + name), TypesTreeView.class);
-				System.out.println(view.getTypeNode().getClassPath() + view.getTypeNode().getClassName());
+				visit(view);
 			}
+			typesByClassName.entrySet()
+					  .stream()
+					  .map(entry -> entry.getValue())
+					  .forEach(type -> {
+						  type.initMethods(typesByClassName);
+					  });
 		} catch (IOException ex) {
 			Logger.getLogger(BasicProvider.class.getName()).log(Level.SEVERE, null, ex);
+		}
+	}
+
+	private void visit(TypesTreeView tree) {
+		BaseTypeView typeView = tree.getTypeNode();
+		TypeWrapper type = new TypeWrapper(typeView,
+													  typesByClassName.containsKey(typeView.getParent())
+													  ? typesByClassName.get(typeView.getParent())
+													  : null);
+		typesByName.put(type.getClassName(), type);
+		typesByClassName.put(type.getClassPath(), type);
+		if (tree.getInnerTypes() != null) {
+			tree.getInnerTypes().forEach(inner -> {
+				visit(inner);
+			});
 		}
 	}
 
